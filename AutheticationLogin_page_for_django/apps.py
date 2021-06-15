cf3afccc773b6571929de3e860efc79dff070280
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-16 00:19:00 +0100

    login

---
+++ AutheticationLogin_page_for_django/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutheticationloginPageForDjangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AutheticationLogin_page_for_django'
