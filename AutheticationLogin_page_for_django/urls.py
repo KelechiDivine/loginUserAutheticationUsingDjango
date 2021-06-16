0609662965d76e47684f0fad24f6f1e368bb85f4
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-16 02:54:52 +0100

    dashboard redirect created

--- AutheticationLogin_page_for_django/urls.py
+++ AutheticationLogin_page_for_django/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('', views.indexView, name='home'),
     path('dashboard/', views.dashboardView, name='dahsboard'),
-    # path('login/', ),
+    path('login/', LoginView.as_view(), name= 'login_url' ),
     path('register/', views.registerView, name='register_url'),
     # path('logout/', ),
 
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/AutheticationLogin_page_for_django/dashboard/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
